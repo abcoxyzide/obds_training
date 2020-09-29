c30e4be6f8642471c1f948f2ba41f6195cba458a
Phil Xie <phil.xie@ludwig.ox.ac.uk>
2020-09-29 17:05:31 +0100

    completed the pipeline

--- rnaseq_pipeline.py
+++ rnaseq_pipeline.py
@@ -19,7 +19,7 @@ from cgatcore import pipeline as P
 params = P.get_parameters('rnaseq_pipeline.yml')
 
 @follows(mkdir("fastqc"))
-@transform('*fastq.gz', regex(r'(.*).fastq.gz'), r'fastqc/\1_fastqc.html')
+@transform('*fastq.gz', regex(r'(.*)\.fastq\.gz'), r'fastqc/\1_fastqc.html')
 def fastqc(infile, outfile):
     statement = "fastqc --nogroup -o fastqc %(infile)s"
     P.run(statement, job_queue='all.q', job_threads=1, job_memory='2G', job_condaenv='obds-py3')
@@ -31,7 +31,7 @@ def multiqc(infiles, outfile):
 
 @follows(mkdir("bam"))
 # @collate( input, filter, output, [extras,...] )
-@collate('*.fastq.gz', regex(r'(.*)_[12].fastq.gz'), r'bam/\1.bam')
+@collate('*.fastq.gz', regex(r'(.*)_[12](.*)\.fastq\.gz'), r'bam/\1\2.bam')
 def mapping(infiles, outfile):
     read1, read2 = infiles
     statement = """
@@ -67,6 +67,12 @@ def featcount(infiles, outfile):
     -o %(outfile)s
     """
     P.run(statement, job_queue='all.q', job_threads=params["featureCounts_threads"], job_memory='2G', job_condaenv='obds-py3')
+
+@merge([idxstats, flagstat, featcount], 'bam/multiqc_report_bam.html')
+def bamultiqc(infiles, outfile):
+    statement = "multiqc -f -n %(outfile)s bam"
+    P.run(statement, job_queue='all.q', job_threads=1, job_memory='2G', job_condaenv='obds-py3')
+
 
 if __name__ == '__main__':
     sys.exit(P.main(sys.argv))
